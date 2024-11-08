1eb900aa90f731158fd5082cc7f66f2aaeb3a8b7
saividya024 <chsaividya180305@gmail.com>
2024-11-08 21:30:10 +0530

    Add files via upload

---
+++ DOM2.js
@@ -0,0 +1,180 @@
+// var card=document.querySelector("section")
+// card.id="cardname"
+// card.innerHTML=`
+// <img src="https://www.akc.org/wp-content/uploads/2017/12/Nova-Scotia-Duck-Tolling-Retriever.jpg" width="300"/>
+// <p> Nova Scotia Duck Tolling Retriever </p>
+// <button> seemore </button> 
+// `
+// card.style.border="2px solid black"
+// card.style.backgroundColor="lightgray"
+// card.style.display="inline-block"
+// card.style.padding="15px"
+// card.style.textAlign="center"
+// document.body.appendChild(card)
+
+// var card=document.querySelector("div")
+// card.id="cardname"
+// card.innerHTML=`
+// <img src="https://www.akc.org/wp-content/uploads/2017/12/Nova-Scotia-Duck-Tolling-Retriever.jpg" width="300"/>
+// <p> Nova Scotia Duck Tolling Retriever </p>
+// <button> seemore </button> 
+// `
+// card.style.border="2px solid black"
+// card.style.backgroundColor="lightgray"
+// card.style.display="inline-block"
+// card.style.padding="15px"
+// card.style.textAlign="center"
+// document.body.appendChild(card)
+
+
+// // var newcard=document.createElement("div")
+// // newcard.id="newcardname"
+// // newcard.innerHTML=`
+// // <img src="https://th.bing.com/th?id=OIP.73ClGYHGReevkne1_hVF7AAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"/>
+// // <p> women's handbag </p>
+// // <button> seemore </button> 
+// // `
+// // newcard.style.border="2px solid black"
+// // newcard.style.backgroundColor="lightgray"
+// // newcard.style.display="inline-block"
+// // newcard.style.padding="15px"
+// // newcard.style.textAlign="center"
+// // document.body.appendChild(newcard)
+// // container.appendChild(newcard)
+
+// // container.style.display="flex"
+// // container.style.gap="20px"
+
+// var container=document.querySelector("div")
+// var newcard1=document.createElement("div")
+// newcard1.id="newcardname"
+// newcard1.innerHTML=`
+// <img src="https://th.bing.com/th?id=OIP.73ClGYHGReevkne1_hVF7AAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"/>
+// <p> women's handbag </p>
+// <button> seemore </button> 
+// `
+// newcard1.id="nercard1"
+// newcard1.style.border="2px solid black"
+// newcard1.style.backgroundColor="lightgray"
+// newcard1.style.display="inline-block"
+// newcard1.style.padding="15px"
+// newcard1.style.textAlign="center"
+// // document.body.appendChild(newcard1)
+// container.appendChild(newcard1)
+
+// container.style.display="flex"
+// container.style.gap="20px"
+// // container.style.justifyContent="center"
+// // container.style.flexWrap="wrap"
+// // newcard1.style.border="2px solid black"
+// // container.style.backgroundColor="lightgray"
+// // container.style.display="inline-block"
+// // container.style.padding="11px"
+
+var container=document.querySelector ("div")
+
+var card=document.createElement("div")
+card.id="cardname"
+card.innerHTML=`
+<img src="https://www.akc.org/wp-content/uploads/2017/12/Nova-Scotia-Duck-Tolling-Retriever.jpg" width="300"/>
+<p> Nova Scotia Duck Tolling Retriever </p>
+<button> seemore </button> 
+`
+card.style.border="2px solid black"
+card.style.backgroundColor="lightgray"
+card.style.display="inline-block"
+card.style.padding="15px"
+card.style.margin="5px"
+card.style.textAlign="center"
+
+container.appendChild(card)
+
+var card=document.querySelector("section")
+card.id="cardname"
+card.innerHTML=`
+<img src="https://th.bing.com/th?id=OIP.73ClGYHGReevkne1_hVF7AAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" width="300"/>
+<p> Nova Scotia Duck Tolling Retriever </p>
+<button> seemore </button> 
+`
+card.style.border="2px solid black"
+card.style.backgroundColor="lightgray"
+card.style.display="inline-block"
+card.style.padding="15px"
+card.style.margin="5px"
+card.style.textAlign="center"
+
+document.body.appendChild(card)
+
+
+
+var card=document.querySelector("p")
+card.id="cardname"
+card.innerHTML=`
+<img src="https://www.akc.org/wp-content/uploads/2017/12/Nova-Scotia-Duck-Tolling-Retriever.jpg" width="300"/>
+<p> Nova Scotia Duck Tolling Retriever </p>
+<button> seemore </button> 
+`
+card.style.border="2px solid black"
+card.style.backgroundColor="lightgray"
+card.style.display="inline-block"
+card.style.padding="15px"
+card.style.margin="5px"
+card.style.textAlign="center"
+
+document.body.appendChild(card)
+
+var newcard=document.createElement("div")
+newcard.id="newcardname"
+newcard.innerHTML=`
+<img src="https://th.bing.com/th?id=OIP.73ClGYHGReevkne1_hVF7AAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" width="300"/>
+<p> Roller Coasters While Pregnant </p>
+<button> seemore </button> 
+`
+newcard.style.border="2px solid black"
+newcard.style.backgroundColor="lightgray"
+newcard.style.display="inline-block"
+newcard.style.padding="15px"
+newcard.style.margin="5px"
+newcard.style.textAlign="center"
+container.appendChild(newcard)
+
+
+
+// var container=document.querySelector ("div")
+
+// var card=document.createElement("div")
+// card.id="cardname"
+// card.innerHTML=`
+// <img src="https://www.akc.org/wp-content/uploads/2017/12/Nova-Scotia-Duck-Tolling-Retriever.jpg" width="300"/>
+// <p> Nova Scotia Duck Tolling Retriever </p>
+// <button> seemore </button> 
+// `
+// card.style.border="2px solid black"
+// card.style.backgroundColor="lightgray"
+// card.style.display="inline-block"
+// card.style.padding="15px"
+// card.style.margin="5px"
+// card.style.textAlign="center"
+
+// container.appendChild(card)
+
+
+
+// var newcard=document.createElement("div")
+// newcard.id="newcardname"
+// newcard.innerHTML=`
+// <img src="https://th.bing.com/th?id=OIP.73ClGYHGReevkne1_hVF7AAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" width="300"/>
+// <p> Roller Coasters While Pregnant </p>
+// <button> seemore </button> 
+// `
+// newcard.style.border="2px solid black"
+// newcard.style.backgroundColor="lightgray"
+// newcard.style.display="inline-block"
+// newcard.style.padding="15px"
+// newcard.style.margin="5px"
+// newcard.style.textAlign="center"
+
+
+// container.appendChild(newcard)
+
+
